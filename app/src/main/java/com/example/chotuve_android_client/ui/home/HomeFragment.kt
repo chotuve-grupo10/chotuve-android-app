84e76e1284d92b178d1155e068f76ca533009488
nicolaslongo <nicolaslongo@github.com>
2020-04-23 15:14:19 -0300

    Comportamiento tonto en el boton, ya no es tan bobo

--- app/src/main/java/com/example/chotuve_android_client/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/chotuve_android_client/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -22,10 +23,22 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        // Aca estaba el texto de fondo de Home. Lo oculte!
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.ping_button).setOnClickListener {
+            val homeTextView = view.findViewById<TextView>(R.id.text_home)
+            homeTextView.text = "¿Y si me pingueas esta?"
+        }
+
+    }
+
 }
