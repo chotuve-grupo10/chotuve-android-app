ea85bab1172d39072bb692d7c35338e566739081
nicolaslongo <nicolaslongo@github.com>
2020-05-09 23:10:01 -0300

    agrego archivos, aun no logra importar Firebase

---
+++ app/src/main/java/com/example/chotuve_android_client/ui/upload_video/UploadVideo.kt
@@ -0,0 +1,91 @@
+package com.example.chotuve_android_client.ui.upload_video
+
+import android.app.Activity.RESULT_OK
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.chotuve_android_client.R
+import com.google.firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.util.*
+
+val PICK_IMAGE_CODE = 1000;         // Por qué ?!?
+
+class UploadVideo : Fragment() {
+
+    private lateinit var uploadVideoViewModel: UploadVideoViewModel
+    private lateinit var alertDialog: AlertDialog.Builder
+    private lateinit var storageReference: StorageReference
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        alertDialog = AlertDialog.Builder(this.getContext())
+        uploadVideoViewModel =
+                ViewModelProviders.of(this).get(UploadVideoViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // FireBase button
+        view.findViewById<Button>(R.id.firebase_button).setOnClickListener {
+            val homeTextView = view.findViewById<TextView>(R.id.text_home)
+            homeTextView.text = "Getting Firebase storage instance"
+
+            // Init op
+            getFileFromGallery(view);
+        }
+    }
+
+    fun getFileFromGallery(view: View) {
+        val i = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        val RESULT_LOAD_IMAGE = 0;
+        startActivityForResult(i, 0);
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val RESULT_LOAD_IMAGE = 0;
+            if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+                val selectedImage: Uri = data.data!!
+                // uploadToFirebase(selectedImage)
+                alertDialog.setMessage("Apparently, I have the file!")
+                uploadToFirebase(selectedImage)
+            } else {
+                // handle error
+                alertDialog.setMessage("Obviously something went wrong")
+            }
+        alertDialog.show()
+    }
+
+    fun uploadToFirebase(videoUri : Uri) {
+        this.getContext()?.let {
+            FirebaseApp.initializeApp(it)
+            Log.i("firebase", "Firebase App initialized")
+        };
+        val firebaseInstance = FirebaseStorage.getInstance("gs://chotuve-android-app.appspot.com")
+        storageReference = firebaseInstance.getReference("upload_test/" + UUID.randomUUID())
+        val uploadTask = storageReference.putFile(videoUri)
+    }
+
+}
---
+++ app/src/main/java/com/example/chotuve_android_client/ui/upload_video/UploadVideoModel.kt
@@ -0,0 +1,13 @@
+package com.example.chotuve_android_client.ui.upload_video
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class UploadVideoViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "In here you can upload your video"
+    }
+    val text: LiveData<String> = _text
+}
