3c574834e946f8b9332e5c9f7717c87876ac430f
nicolaslongo <nicolaslongo@github.com>
2020-06-01 18:27:42 -0300

    agrego una nueva actividad PlayVideoActivity

---
+++ app/src/main/java/com/example/chotuve_android_client/ui/playVideo/PlayVideoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chotuve_android_client.ui.playVideo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chotuve_android_client.R
+
+class PlayVideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_video)
+    }
+}
