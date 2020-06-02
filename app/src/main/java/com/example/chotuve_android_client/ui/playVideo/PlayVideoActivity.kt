e577c07cf95bdfb6ae068dbdcf543ce9bd5faee8
nicolaslongo <nicolaslongo@github.com>
2020-06-02 00:43:59 -0300

    No logramos lo que queriamos pero... podemos ver el video directo de internet

--- app/src/main/java/com/example/chotuve_android_client/ui/playVideo/PlayVideoActivity.kt
+++ app/src/main/java/com/example/chotuve_android_client/ui/playVideo/PlayVideoActivity.kt
@@ -1,5 +1,6 @@
 package com.example.chotuve_android_client.ui.playVideo
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +11,8 @@ import androidx.lifecycle.ViewModelProviders
 import com.example.chotuve_android_client.R
 import com.example.chotuve_android_client.data.model.Video
 import com.example.chotuve_android_client.ui.home.HomeViewModel
+import java.io.File
+import java.io.FileNotFoundException
 
 class PlayVideoActivity : AppCompatActivity() {
 
@@ -19,7 +22,6 @@ class PlayVideoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_video)
-
 
         val video : Video? = intent.getParcelableExtra<Video>("video_to_play")
         if (video != null) {
@@ -33,7 +35,24 @@ class PlayVideoActivity : AppCompatActivity() {
                 textView.text = it
             })
             val videoView : VideoView = this.findViewById(R.id.videoView)
-            playVideoViewModel.videoView.observe()
+             Log.d("playvid", "Soy la PlayVideoActivity y estoy bindeando el filePath")
+            playVideoViewModel.filePath.observe ( this, Observer<String> {
+                try{
+                    var file : File = File(it)
+                    if(file.exists()) {
+                        Log.d("playvid", "OTRA VEZ soy la PlayVideoActivity y estoy bindeando el filePath")
+                        videoView.setVideoURI(Uri.parse(video.url))
+                        videoView.start()
+                    }
+                } catch(e: FileNotFoundException)
+                {
+                    Log.d("playvid", "ESTA VEZ soy la PlayVideoActivity y el bind del filePath fallo")
+                }
+            })
+//
+//            val filePath : String = playVideoViewModel.getFilePath()
+//            videoView.setVideoPath(filePath)
+////            playVideoViewModel.videoView.observe()
         }
     }
 
--- app/src/main/java/com/example/chotuve_android_client/ui/playVideo/PlayVideoViewModel.kt
+++ app/src/main/java/com/example/chotuve_android_client/ui/playVideo/PlayVideoViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.chotuve_android_client.data.model.Video
+import com.google.firebase.storage.FileDownloadTask
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import java.io.File
@@ -15,47 +16,49 @@ import java.lang.Exception
 // TODO: env var this
 val APPSPOT_URL = "gs://chotuve-android-app.appspot.com";
 
+
 class PlayVideoViewModel (private val video: Video) : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = video.title
+        this.value = video.title
     }
     val text: LiveData<String> = _text
-
     private lateinit var storageReference: StorageReference
 
-    // Acá haré algo con el video.
+    val absolutPath = downLoadVideo(video)
+    val _filePath = MutableLiveData<String>().apply {
+        this.value = absolutPath
+    }
+    val filePath : LiveData<String> = _filePath
+
     init {
-        Log.d("playvid", "Soy el PlayVideoViewModel y el video es este " + video.title)
+        Log.d("playvid", "Soy el PlayVideoViewModel y el video es este "
 
-        // Hay que ordenar tdodo esto en la mente
-//        private val videoView = VideoView()
-//        val uri = Uri.parse(video.url)
-//        _videoView.setVideoUri(uri)
-//        // setear visibility de una progressBar acá?
-
-        downLoadVideo(video)
+        + video.title + ". La URL es esta" + video.url)
     }
 
-    private fun downLoadVideo(video: Video){
+
+        // Se la estoy asignando a un videoView que no existe. Cómo hago que esto sea mutable?
+        // Claro que esto no funcionó
+//        videoView.setVideoPath(absolutPath)
+//        _videoView.value = videoView
+
+    private fun downLoadVideo(video: Video) : String {
         val firebaseInstance = FirebaseStorage.getInstance(APPSPOT_URL);
         storageReference = firebaseInstance.getReferenceFromUrl(video.url);
-
-        Log.d("playvid","URL is:" + video.url);
-
         val localFile = File.createTempFile("video", ".mp4")
-
         // Esta opcion es para descargar el video en la carpeta Downloads del celu
         //val localFile = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "video.mp4")
-
-        storageReference.getFile(localFile).addOnSuccessListener {
+        val task = storageReference.getFile(localFile)
+        task.addOnSuccessListener {
             // Local temp file has been created
             Log.d("playvid","Video downloaded!");
-            Log.d("playvid","path: " + localFile.absolutePath);
+            Log.d("playvid","path: " + localFile.absolutePath)
 
         }.addOnFailureListener {
             // Handle any errors
             Log.e("playvid","Error downloading video");
         }
+        return localFile.absolutePath
     }
 }
