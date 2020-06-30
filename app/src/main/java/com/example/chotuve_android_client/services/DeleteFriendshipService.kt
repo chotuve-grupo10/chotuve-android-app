3afaa0045996fe1197700336fd82062fa00d0ab5
nicolaslongo <nicolaslongo@github.com>
2020-06-30 18:24:37 -0300

    agrego logica de servicio para eliminar amigos

---
+++ app/src/main/java/com/example/chotuve_android_client/services/DeleteFriendshipService.kt
@@ -0,0 +1,30 @@
+package com.example.chotuve_android_client.services
+
+import com.example.chotuve_android_client.apis.DefaultApi
+import com.example.chotuve_android_client.models.BasicServerResponse
+import com.example.chotuve_android_client.tools.RetrofitObject
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+
+class DeleteFriendshipService {
+
+    private val retrofit : Retrofit = RetrofitObject.retrofit
+    private val deleteFriendshipService = retrofit.create(DefaultApi::class.java)
+
+    fun deleteFriendship(
+        user_email : String,
+        new_friends_email : String,
+        disposable: CompositeDisposable?,
+        onSuccess: (users: BasicServerResponse?) -> Unit,
+        onError: (throwable: Throwable) -> Unit
+    ) {
+        disposable?.add(deleteFriendshipService.apiUsersUserEmailFriendsFriendsEmailDelete(user_email, new_friends_email)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe(onSuccess, onError))
+
+    }
+
+}
--- app/src/main/java/com/example/chotuve_android_client/tools/adapters/FriendsAdapter.kt
+++ app/src/main/java/com/example/chotuve_android_client/tools/adapters/FriendsAdapter.kt
@@ -1,19 +1,30 @@
 package com.example.chotuve_android_client.tools.adapters
 
+import android.app.AlertDialog
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chotuve_android_client.models.UsersInformationList
 import com.example.chotuve_android_client.databinding.RecyclerviewFriendsBinding
 import com.example.chotuve_android_client.R
+import com.example.chotuve_android_client.services.DeleteFriendshipService
+import com.example.chotuve_android_client.tools.TokenHolder
+import com.example.chotuve_android_client.tools.error_handlers.ServerMessageHttpExceptionHandler
+import io.reactivex.disposables.CompositeDisposable
+import retrofit2.HttpException
 
 class FriendsAdapter(
     val users : UsersInformationList
-) : RecyclerView.Adapter<FriendsAdapter.UserViewHolder>() {
+) : RecyclerView.Adapter<FriendsAdapter.FriendsViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        return UserViewHolder(
+    val TAG = "FriendsAdapter"
+    val FRIENDSHIP_DELETE = "Friendship_delete"
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendsViewHolder {
+        return FriendsViewHolder(
             DataBindingUtil.inflate(
                 LayoutInflater.from(parent.context),
                 R.layout.recyclerview_friends,
@@ -23,21 +34,47 @@ class FriendsAdapter(
         )
     }
 
-    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: FriendsViewHolder, position: Int) {
         holder.recyclerviewFriendsBinding.user = users[position]
-        // setOnClickListener
-        // Here I can add clickListeners to different elements in coso
-//        holder.recyclerviewVideoBinding.videoTitle.setOnClickListener { view ->
-//            val ad = AlertDialog.Builder(view.context)
-//            ad.setMessage("You clicked on " + videos[position].title)
-//            ad.show()
-//        }
 
+        // Here I can add clickListeners to different elements in coso
+        holder.recyclerviewFriendsBinding.buttonDeleteFriend.setOnClickListener { view ->
+            val ad = AlertDialog.Builder(view.context)
+            Log.d(TAG, "User about to remove this friend " + users[position].fullName)
+            deleteFriendship(view,
+                TokenHolder.username,
+                users[position].email.toString()
+            )
+        }
     }
 
     override fun getItemCount() = users.size
 
-    inner class UserViewHolder(
+    fun deleteFriendship(view: View, user_email : String, friends_email : String) {
+        val deleteFriendshipService = DeleteFriendshipService()
+        deleteFriendshipService.deleteFriendship(
+            user_email,
+            friends_email,
+            CompositeDisposable(),
+            {
+                val ad = AlertDialog.Builder(view.context)
+                ad.setMessage("Usuario" + friends_email + " eliminado de la lista de amigos con éxito")
+                ad.show()
+            },
+            {
+                it.printStackTrace()
+                Log.d(TAG, "Error sending friendship request: ${it.localizedMessage}")
+                if (it is HttpException) {
+                    val error = ServerMessageHttpExceptionHandler(it, FRIENDSHIP_DELETE)
+                    val ad = androidx.appcompat.app.AlertDialog.Builder(view.context)
+                    ad.setMessage(error.message)
+                    ad.show()
+                }
+            }
+        )
+    }
+
+    inner class FriendsViewHolder(
         val recyclerviewFriendsBinding : RecyclerviewFriendsBinding
     ) : RecyclerView.ViewHolder(recyclerviewFriendsBinding.root)
 }
