1bba1f2ea81cb1ed0d4fe844d8b0c67f4929f804
nicolaslongo <nicolaslongo@github.com>
2020-07-05 18:25:53 -0300

    agregadosr

--- app/src/main/java/com/example/chotuve_android_client/MainActivity.kt
+++ app/src/main/java/com/example/chotuve_android_client/MainActivity.kt
@@ -2,15 +2,17 @@ package com.example.chotuve_android_client
 
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.chotuve_android_client.tools.TokenHolder
-import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.InstanceIdResult
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val messagingService = MyFirebaseMessagingService()
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
@@ -29,21 +32,14 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        Log.d("MainActivity", "App server token is ${TokenHolder.appServerToken}")
+        Log.d(TAG, "App server token is ${TokenHolder.appServerToken}")
 
-        FirebaseInstanceId.getInstance().instanceId
-            .addOnCompleteListener(OnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    Log.w(TAG, "getInstanceId failed", task.exception)
-                    return@OnCompleteListener
-                }
-                // Get new Instance ID token
-                val token = task.result?.token
-                // Log and toast
-                Log.d(TAG, "Your MyFirebaseMS token is " + token.toString())
+        FirebaseInstanceId.getInstance().instanceId.addOnSuccessListener(
+            this@MainActivity,
+            OnSuccessListener<InstanceIdResult> { instanceIdResult ->
+                val token = instanceIdResult.token
+                Log.d(TAG, "El token en ${TAG} es ${token}")
             })
-
-        MyFirebaseMessagingService()
 
     }
 
