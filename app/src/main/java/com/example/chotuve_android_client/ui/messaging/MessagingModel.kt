f4177f368c4230245580af54827560dbb19b8e7c
nicolaslongo <nicolaslongo@github.com>
2020-07-12 17:25:25 -0300

    Agrego codigo siguiendo un approach de Threads usando c´ódigo de Apache. No lo pude hacer andar

--- app/src/main/java/com/example/chotuve_android_client/ui/messaging/MessagingModel.kt
+++ app/src/main/java/com/example/chotuve_android_client/ui/messaging/MessagingModel.kt
@@ -9,12 +9,33 @@ import androidx.lifecycle.MutableLiveData
 import com.example.chotuve_android_client.data.MessagesRepository
 import com.example.chotuve_android_client.models.Message
 import com.example.chotuve_android_client.tools.TokenHolder
+import org.apache.commons.net.ntp.NTPUDPClient
+import org.apache.commons.net.ntp.TimeInfo
+import java.net.InetAddress
 import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.concurrent.thread
 
 
 class MessagingModel {
+
+    companion object TimeLookup {
+        const val TAG = "ObjTimeLookup"
+        const val TIME_SERVER = "time-a.nist.gov"
+
+        @Throws(Exception::class)
+        @JvmStatic
+        fun main() {
+            val timeClient = NTPUDPClient()
+            val inetAddress: InetAddress = InetAddress.getByName(TIME_SERVER)
+            val timeInfo: TimeInfo = timeClient.getTime(inetAddress)
+            val returnTime: Long = timeInfo.getReturnTime()
+            val time = Date(returnTime)
+            Log.d(TAG, "Time from $TIME_SERVER: $time")
+        }
+    }
 
     val TAG = "MessagingModel"
     val messagesRepository = MessagesRepository()
@@ -26,6 +47,11 @@ class MessagingModel {
     @RequiresApi(Build.VERSION_CODES.O)
     fun sendNewMessage(editText : Editable, userIdReceivingMessage : String) {
         Log.d(TAG, "Nuevo mensaje es $editText de ${TokenHolder.username} y es para $userIdReceivingMessage")
+
+        thread {
+            val timeLookUp = TimeLookup
+            timeLookUp.main()
+        }
 
         val currentDateTime = DateTimeFormatter
             .ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS")
@@ -48,3 +74,5 @@ class MessagingModel {
     }
 
 }
+
+
