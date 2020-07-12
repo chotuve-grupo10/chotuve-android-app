726ac66445cf3bbfaa627ca5b945dd7bb3e1ef9c
nicolaslongo <nicolaslongo@github.com>
2020-07-12 19:07:22 -0300

    Agrego servicio que pide la hora al server

--- app/src/main/java/com/example/chotuve_android_client/ui/messaging/MessagingModel.kt
+++ app/src/main/java/com/example/chotuve_android_client/ui/messaging/MessagingModel.kt
@@ -8,7 +8,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.chotuve_android_client.data.MessagesRepository
 import com.example.chotuve_android_client.models.Message
+import com.example.chotuve_android_client.services.GetServerTimeService
 import com.example.chotuve_android_client.tools.TokenHolder
+import io.reactivex.disposables.CompositeDisposable
 import org.apache.commons.net.ntp.NTPUDPClient
 import org.apache.commons.net.ntp.TimeInfo
 import java.net.InetAddress
@@ -21,21 +23,21 @@ import kotlin.concurrent.thread
 
 class MessagingModel {
 
-    companion object TimeLookup {
-        const val TAG = "ObjTimeLookup"
-        const val TIME_SERVER = "time-a.nist.gov"
-
-        @Throws(Exception::class)
-        @JvmStatic
-        fun main() {
-            val timeClient = NTPUDPClient()
-            val inetAddress: InetAddress = InetAddress.getByName(TIME_SERVER)
-            val timeInfo: TimeInfo = timeClient.getTime(inetAddress)
-            val returnTime: Long = timeInfo.getReturnTime()
-            val time = Date(returnTime)
-            Log.d(TAG, "Time from $TIME_SERVER: $time")
-        }
-    }
+//    companion object TimeLookup {
+//        const val TAG = "ObjTimeLookup"
+//        const val TIME_SERVER = "time-a.nist.gov"
+//
+//        @Throws(Exception::class)
+//        @JvmStatic
+//        fun main() {
+//            val timeClient = NTPUDPClient()
+//            val inetAddress: InetAddress = InetAddress.getByName(TIME_SERVER)
+//            val timeInfo: TimeInfo = timeClient.getTime(inetAddress)
+//            val returnTime: Long = timeInfo.getReturnTime()
+//            val time = Date(returnTime)
+//            Log.d(TAG, "Time from $TIME_SERVER: $time")
+//        }
+//    }
 
     val TAG = "MessagingModel"
     val messagesRepository = MessagesRepository()
@@ -48,21 +50,13 @@ class MessagingModel {
     fun sendNewMessage(editText : Editable, userIdReceivingMessage : String) {
         Log.d(TAG, "Nuevo mensaje es $editText de ${TokenHolder.username} y es para $userIdReceivingMessage")
 
-        thread {
-            val timeLookUp = TimeLookup
-            timeLookUp.main()
-        }
-
-        val currentDateTime = DateTimeFormatter
-            .ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS")
-            .withZone(ZoneId.of("AGT"))
-            .format(Instant.now())
+        val currentDateTime = getCurrentDateTime()
 
         val message = hashMapOf(
             "fromId" to TokenHolder.username,
             "toId" to userIdReceivingMessage,
             "text" to editText.toString(),
-            "timestamp" to currentDateTime
+            "timestamp" to currentDateTime.toString()
         )
 
         messagesRepository.sendMessage(message)
@@ -71,6 +65,29 @@ class MessagingModel {
     fun getMessages(userIdReceivingMessage : String) {
 
         messagesRepository.getMessagesFromFirestore(userIdReceivingMessage, _messages)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getCurrentDateTime() : String? {
+
+        var currentDateTime : String? = null
+
+        val getServerTimeService = GetServerTimeService()
+        getServerTimeService.getServerTime(
+            CompositeDisposable(),
+            {
+                currentDateTime = it!!.timestamp
+            }, {
+                it.printStackTrace()
+                Log.d(TAG, "Error getting time from AppServer: ${it.localizedMessage}")
+                currentDateTime = DateTimeFormatter
+                    .ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS")
+                    .withZone(ZoneId.of("AGT"))
+                    .format(Instant.now())
+            }
+        )
+        Log.d(TAG, "CurrentDateTime es ${currentDateTime}")
+        return currentDateTime
     }
 
 }
