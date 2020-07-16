4c69bd0494028c8d99403bacad6a913c492f5682
nicolaslongo <nicolaslongo@github.com>
2020-07-16 16:53:40 -0300

    Lista la logica de mensajes por el momento con la hora del celular local

--- app/src/main/java/com/example/chotuve_android_client/ui/messaging/MessagingModel.kt
+++ app/src/main/java/com/example/chotuve_android_client/ui/messaging/MessagingModel.kt
@@ -35,6 +35,26 @@ class MessagingModel : ViewModel() {
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
+    fun sendNewMessage(editText : Editable, userIdReceivingMessage : String) {
+
+        val currentDateTime = DateTimeFormatter
+            .ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS")
+            .withZone(ZoneId.of("AGT"))
+            .format(Instant.now())
+        Log.d(TAG, "CurrentDateTime del mensaje ${currentDateTime}")
+
+        val message = hashMapOf(
+            "fromId" to TokenHolder.username,
+            "toId" to userIdReceivingMessage,
+            "text" to editText.toString(),
+            "timestamp" to currentDateTime.toString()
+        )
+        messagesRepository.sendMessage(message)
+    }
+
+/*
+// Acá dejo los intentos de usar la hora del server todos ellos fallidos tan lleno de sueños rotos //
+    @RequiresApi(Build.VERSION_CODES.O)
 //    suspend fun sendNewMessage(editText : Editable, userIdReceivingMessage : String)  {
     suspend fun getServerTime() : String? {
         var currentDateTime : String? = null
@@ -56,9 +76,6 @@ class MessagingModel : ViewModel() {
                 Log.d(TAG, "CurrentDateTime del mensaje ${currentDateTime}")
             }
         )
-//        val job = GlobalScope.async {
-//        }
-//        job.join()
         Log.d(TAG, "CurrentDateTime del mensaje ${currentDateTime}")
         return currentDateTime
     }
@@ -69,7 +86,7 @@ class MessagingModel : ViewModel() {
     runBlocking {
         Log.d(TAG, "Nuevo mensaje es $editText de ${TokenHolder.username} y es para $userIdReceivingMessage")
         launch(Dispatchers.IO) {
-            val currentDateTime : String? = getServerTime()
+            val currentDateTime : String? = async { getServerTime() }.await()
             val message = hashMapOf(
                 "fromId" to TokenHolder.username,
                 "toId" to userIdReceivingMessage,
@@ -79,6 +96,7 @@ class MessagingModel : ViewModel() {
             messagesRepository.sendMessage(message)
         }
     }
+*/
 }
 
 
