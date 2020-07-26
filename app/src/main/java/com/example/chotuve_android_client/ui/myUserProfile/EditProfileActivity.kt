d42b4763e3514db1a9763b0778130cc1e29f629b
nicolaslongo <nicolaslongo@github.com>
2020-07-26 19:10:54 -0300

    Tire por la ventana el tema de las fotos de perfil

--- app/src/main/java/com/example/chotuve_android_client/ui/myUserProfile/EditProfileActivity.kt
+++ app/src/main/java/com/example/chotuve_android_client/ui/myUserProfile/EditProfileActivity.kt
@@ -1,23 +1,32 @@
 package com.example.chotuve_android_client.ui.myUserProfile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import com.example.chotuve_android_client.R
+import com.example.chotuve_android_client.ui.uploadVideo.RESULT_LOAD_VIDEO
+import com.squareup.picasso.Picasso
 
 class EditProfileActivity : AppCompatActivity() {
 
+    private lateinit var alertDialogBuilder: android.app.AlertDialog.Builder
     private val editProfileViewModel = EditProfileViewModel()
-    val TAG = "EditProfileAct"
+    private val TAG = "EditProfileAct"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
+        // TODO: puede que tenga que inicializar FirebaseApp
+        alertDialogBuilder = android.app.AlertDialog.Builder(this)
 
         editProfileViewModel.getProfileInformation()
 
@@ -28,10 +37,22 @@ class EditProfileActivity : AppCompatActivity() {
 
         val phoneNumber = findViewById<EditText>(R.id.editable_phone_number)
         val fullName = findViewById<EditText>(R.id.editable_full_name)
+        val profilePicture = findViewById<ImageView>(R.id.profile_picture)
         editProfileViewModel.listed_user.observe(this, Observer {
-            phoneNumber.setText( it.phoneNumber.toString() )
-            fullName.setText( it.fullName.toString() )
+            phoneNumber.setText(it.phoneNumber.toString())
+            fullName.setText(it.fullName.toString())
+            if (it.profilePicture != "") {
+                Picasso
+                    .get()
+                    .load(it.profilePicture)
+                    .into(profilePicture)
+            }
         })
+
+        val uploadProfilePicture = findViewById<Button>(R.id.button_upload_picture)
+        uploadProfilePicture.setOnClickListener {
+            getProfilePictureFromGallery()
+        }
 
         val sendChanges = findViewById<Button>(R.id.button_send_profile_editions)
         sendChanges.setOnClickListener {
@@ -52,4 +73,16 @@ class EditProfileActivity : AppCompatActivity() {
 
     }
 
+    fun getProfilePictureFromGallery() {
+        startActivityForResult(
+            editProfileViewModel.getFileFromGallery(),
+            EditProfileViewModel.RESULT_LOAD_IMAGE
+        )
+    }
+
+    // For the ActivityResult:
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        editProfileViewModel.dispatcherToFirebase(requestCode, resultCode, data, alertDialogBuilder)
+    }
 }
--- app/src/main/java/com/example/chotuve_android_client/ui/myUserProfile/EditProfileViewModel.kt
+++ app/src/main/java/com/example/chotuve_android_client/ui/myUserProfile/EditProfileViewModel.kt
@@ -1,27 +1,48 @@
 package com.example.chotuve_android_client.ui.myUserProfile
 
+import android.app.Activity
+import android.content.Intent
 import android.media.session.MediaSession
+import android.net.Uri
+import android.provider.MediaStore
 import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.chotuve_android_client.models.ListedUser
 import com.example.chotuve_android_client.services.UserProfileService
+import com.example.chotuve_android_client.tools.FirebaseStorageHandler
 import com.example.chotuve_android_client.tools.TokenHolder
 import com.example.chotuve_android_client.ui.recovery.RecoveryModel
+import com.example.chotuve_android_client.ui.uploadVideo.RESULT_LOAD_VIDEO
+import com.example.chotuve_android_client.ui.uploadVideo.UploadVideoViewModel
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import io.reactivex.disposables.CompositeDisposable
+import java.util.*
 
 class EditProfileViewModel {
+    // TODO: env var these
+    val IMAGE_LOCATION = "profile_pictures/"
+    val APPSPOT_URL = "gs://chotuve-android-app.appspot.com"
+    val TAG = "EditProfileViewModel"
+
+    private lateinit var storageReference: StorageReference
+    private lateinit var uuidFileName: String
+    private lateinit var alertDialog: android.app.AlertDialog
+
+    private val firebaseStorageHandler = FirebaseStorageHandler()
 
     companion object {
         const val STATUS_INIT = 0
         const val STATUS_CHANGED = 1
+        const val RESULT_LOAD_IMAGE = 1
     }
 
-        val TAG = "EditProfileViewModel"
     private val _text = MutableLiveData<String>().apply {
         // TODO: nice to have aca el fullName, o algo más amigable que el mail
-        value = "Tus datos personales, ${TokenHolder.username}. Desde acá podés editar tu información personal"
+        value =
+            "Tus datos personales, ${TokenHolder.username}. Desde acá podés editar tu información personal"
     }
     val text: LiveData<String>
         get() = _text
@@ -29,15 +50,19 @@ class EditProfileViewModel {
     val _status = MutableLiveData<Int>().apply {
         this.value = STATUS_INIT
     }
-    val status : LiveData<Int>
+    val status: LiveData<Int>
         get() = _status
 
 
     private val _listed_user = MutableLiveData<ListedUser>().apply {
         this.value = ListedUser(TokenHolder.username, "Nombre", "Número de teléfono", "")
     }
-    val listed_user : LiveData<ListedUser>
+    val listed_user: LiveData<ListedUser>
         get() = _listed_user
+
+    private val _urlUploaded = MutableLiveData<String>().apply {
+        this.value = _listed_user.value!!.profilePicture.toString()
+    }
 
 
     fun getProfileInformation() {
@@ -52,43 +77,117 @@ class EditProfileViewModel {
             },
             {
                 it.printStackTrace()
-                Log.e(TAG, "No se pudo recibir correctamente la información de usuario: " + it.message)
+                Log.e(
+                    TAG,
+                    "No se pudo recibir correctamente la información de usuario: " + it.message
+                )
             }
         )
     }
 
-    fun sendNewProfileInformation(fullName : String, phoneNumber : String, ad : AlertDialog.Builder) {
-        if( changesHaveBeenMade(fullName, phoneNumber) ) {
-            Log.d(TAG, "New fullname ${fullName}, new phoneNumber ${phoneNumber}")
-            UserProfileService()
-                .putNewUserDataInformation(
-                    TokenHolder.appServerToken,
-                    TokenHolder.username,
-                    ListedUser(_listed_user.value!!.email, fullName, phoneNumber, _listed_user.value!!.profilePicture),
-                    CompositeDisposable(),
-                    {
-                        if (it != null) {
-                            _status.value = STATUS_CHANGED
-                        }
-                    },
-                    {
-                        it.printStackTrace()
-                        Log.e(TAG, "No se pudo modificar correctamente la información de usuario: " + it.message)
-                        ad.setMessage("Ocurrió un error")
-                        ad.show()
+    fun sendNewProfileInformation(fullName: String, phoneNumber: String, ad: AlertDialog.Builder) {
+//        if (changesHaveBeenMade(fullName, phoneNumber)) {
+        Log.d(TAG, "New fullname ${fullName}, new phoneNumber ${phoneNumber}")
+        UserProfileService()
+            .putNewUserDataInformation(
+                TokenHolder.appServerToken,
+                TokenHolder.username,
+                ListedUser(
+                    _listed_user.value!!.email,
+                    fullName,
+                    phoneNumber,
+                    _listed_user.value!!.profilePicture
+                ),
+                CompositeDisposable(),
+                {
+                    if (it != null) {
+                        _status.value = STATUS_CHANGED
                     }
-                )
+                },
+                {
+                    it.printStackTrace()
+                    Log.e(
+                        TAG,
+                        "No se pudo modificar correctamente la información de usuario: " + it.message
+                    )
+                    ad.setMessage("Ocurrió un error")
+                    ad.show()
+                }
+            )
+    }
+//        } else {
+//            ad.setMessage("No hay cambios que guardar!")
+//            ad.show()
+//        }
+
+//    fun changesHaveBeenMade(fullName: String, phoneNumber: String): Boolean {
+//        if ((fullName != _listed_user.value!!.fullName)
+//            || (phoneNumber != _listed_user.value!!.phoneNumber) ||
+//                (_urlUploaded)) {
+//            return true
+//        } else {
+//            return false
+//        }
+//    }
+
+    fun getFileFromGallery() =
+        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+    fun dispatcherToFirebase(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?,
+        alertDialogBuilder: android.app.AlertDialog.Builder
+    ) {
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == Activity.RESULT_OK && null != data) {
+            val selectedImage: Uri = data.data!!
+            alertDialogBuilder.setMessage("Aguarde un instante. Subiendo la imagen...")
+            uploadImageToFirebase(selectedImage, alertDialogBuilder)
         } else {
-            ad.setMessage("No hay cambios que guardar!")
-            ad.show()
+            alertDialogBuilder.setMessage("Parece que no elegiste ningún archivo.. No importa, podes intentarlo de nuevo")
         }
+        alertDialog = alertDialogBuilder.show()
     }
 
-    fun changesHaveBeenMade(fullName : String, phoneNumber : String) : Boolean {
-        if( (fullName != _listed_user.value!!.fullName) || (phoneNumber != _listed_user.value!!.phoneNumber) ) {
-            return true
-        } else {
-            return false
+    fun uploadImageToFirebase(selectedImage: Uri, ad: android.app.AlertDialog.Builder) {
+        val storageReference: StorageReference
+
+        Log.d(TAG, "Firebase App initialized")
+        val firebaseInstance = FirebaseStorage.getInstance(APPSPOT_URL)
+
+        val uuidFileName = UUID.randomUUID().toString()
+        storageReference = firebaseInstance.getReference(IMAGE_LOCATION + uuidFileName)
+        val uploadTask = storageReference.putFile(selectedImage)
+
+        uploadTask.continueWithTask { task ->
+            if (!task.isSuccessful) {
+                task.exception?.let {
+                    throw it
+                }
+            }
+            storageReference.downloadUrl
+        }.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                // alertDialog.cancel()
+                val url = task.result
+                _urlUploaded.value = url!!.toString()
+                Log.d(TAG, "This is Firebase Link for Profile Picture ${_urlUploaded.value}")
+                _listed_user.value = ListedUser(
+                    _listed_user.value!!.email,
+                    _listed_user.value!!.fullName,
+                    _listed_user.value!!.phoneNumber,
+                    _urlUploaded.value)
+                alertDialog.dismiss()
+                alertDialog =
+                    ad.setMessage("Listo! Subimos la imagen correctamente. No te olvides de apretar el botón rojo para aceptar tus cambios!")
+                        .show()
+            } else {
+                Log.d(TAG,"Error with Firebase. Apparently the video could not be sent")
+                alertDialog.dismiss()
+                alertDialog =
+                    ad.setMessage("Ups. Algo salió mal con uno de nuestros proveedores de servicios. Porfavor, ¿podrías intentarlo nuevamente?")
+                        .show()
+            }
         }
     }
 
